4cbc78326047008af5f1022351066e89847b1218
7heIVIaze <kimdave20@naver.com>
2022-05-26 21:30:54 +0900

    for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: process.env.npm_package_name + '딱맞아쇼핑몰 - ',
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
