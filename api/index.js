cd1a379bc5b198502906e74f517871369c461f9a
7heIVIaze <kimdave20@naver.com>
2022-05-28 15:46:42 +0900

    test

--- api/index.js
+++ api/index.js
@@ -18,14 +18,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  //res.header("Access-Control-Allow-Origin", "https://192.168.0.6:8080")
+  res.header("Access-Control-Allow-Origin", "https://justright-shop.herokuapp.com") // test http://192.168.0.6:8080
   res.header('Access-Control-Allow-Credentials', true)
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE")
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-  )
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next()
 });
 io.on('connection', function (socket) {
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+var cors = require('cors')
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
+const { VRadio } = require('vuetify/lib')
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start () {
@@ -28,6 +30,17 @@ async function start () {
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://justright-shop.herokuapp.com") // test http://192.168.0.6:8080
+    res.header('Access-Control-Allow-Credentials', false)
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next()
+  });
+  io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' })
+    console.log("connectd")
+  });
   
 
   // Listen the server
