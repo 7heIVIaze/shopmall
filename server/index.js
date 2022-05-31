37f2b56e0145592418311f96802412c0d6c80db9
7heIVIaze <kimdave20@naver.com>
2022-05-31 14:39:16 +0900

    hmm

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ async function start () {
   // Give nuxt middleware to express
   app.use(nuxt.render)
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://172.27.128.1:5000") // test https://justright-shop.herokuapp.com
+    res.header("Access-Control-Allow-Origin", "https://justright-shop.herokuapp.com") // test http://172.27.128.1:5000
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
