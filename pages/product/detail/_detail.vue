977faf64cc6a0726211d46a6c0e40c5f555d89ed
7heIVIaze <kimdave20@naver.com>
2022-06-11 17:55:22 +0900

    test

--- pages/product/detail/_detail.vue
+++ pages/product/detail/_detail.vue
@@ -87,9 +87,7 @@
                     <v-list-item-content>
                       <v-list-item-subtitle>{{ commentItem.commentUserId }} 
                       <span style="float:right;">{{ commentItem.commentDate }}</span></v-list-item-subtitle>
-                      <v-row><v-list-item-subtitle class="mx-0"> 
-                        <span >{{ commentItem.commentRating }}/5</span>
-                         <v-rating
+                      <v-row><v-list-item-subtitle class="mx-0"><pre><v-rating
                               color="yellow darken-3"
                               background-color="grey darken-1"
                               empty-icon="$ratingFull"
@@ -97,8 +95,8 @@
                               readonly
                               size="10"
                               :value="commentItem.commentRating"
-                            ></v-rating>
-                           
+                            ></v-rating></pre>
+                            <span style="float:flex;">{{ commentItem.commentRating }}/5</span>
                       </v-list-item-subtitle></v-row>
                       <v-list-item-title><pre>{{ commentItem.commentContent }}</pre></v-list-item-title>
                     </v-list-item-content>
